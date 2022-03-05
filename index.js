224727703f26e2ef78307927a596ec4d25cd0be6
airfun <ahmedovravshanbek21@gmail.com>
2022-03-06 02:14:29 +0500

    socket 'on's

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
+const {User, Message} =require('./models');
 
 const http = require("http");
 const {Server} = require("socket.io");
@@ -13,7 +14,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 80;
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST"],
     },
 });
@@ -27,10 +28,79 @@ app.use(cors({
 }));
 app.use('/api', router);
 
+// io.on("connection", async (socket) => {
+//     // console.log(socket.id);
+//     socket.on('online', async () => {
+//         // console.count('online-------------------------')
+//     });
+// });
 io.on("connection", async (socket) => {
-    // console.log(socket.id);
-    socket.on('online', async () => {
-        // console.count('online-------------------------')
+    socket.join('room');
+    let userIds = {};
+
+    async function sendMessages() {
+        const newMessages = await Message.find();
+        socket.to('room').emit("backend_sends_message", newMessages);
+        socket.emit("backend_sends_message", newMessages);
+    }
+
+    async function sendUnread() {
+        const unread = await Message.aggregate([
+            {
+                $match: {$and: [{receiverId: userIds[socket.id]}, {deliveryStatus: 1}]}
+            },
+            {
+                $group: {
+                    _id: "$senderId",
+                    count: {$sum: 1}
+                }
+            }
+        ]);
+        socket.emit("backend_sends_unread", unread);
+    }
+
+    async function sendUsers() {
+        const users = await User.find();
+        socket.to('room').emit("backend_sends_user", users);
+        socket.emit("backend_sends_user", users);
+    }
+
+    await sendMessages();
+    await sendUsers();
+    await sendUnread();
+
+    socket.on('frontend_registers', async () => {
+        await sendUsers()
+    });
+    socket.on("frontend_sends_message", async (data) => {
+        await new Message(data).save();
+        await sendMessages();
+        await sendUnread();
+    });
+    socket.on("read_messages", async function (data) {
+        await Message.updateMany({receiverId: data.receiverId, senderId: data.senderId}, {deliveryStatus: 2});
+        await sendMessages();
+        await sendUnread();
+    });
+    socket.on('user_online', async function (data) {
+        // userIds = {...userIds, [socket.id]: data.userId};
+        // await User.updateOne({_id: data.userId}, {isOnline: true, typingTo: ''});
+        // await sendUsers();
+        // await sendUnread()
+    });
+    socket.on('is_typing', async function (data) {
+        await User.updateOne({_id: data.userId}, {typingTo: data.otherId});
+        await sendUsers()
+    });
+    socket.on('is_not_typing', async function (data) {
+        await User.updateOne({_id: data.userId}, {typingTo: ''});
+        await sendUsers()
+    });
+    socket.on("disconnect", async function (e) {
+        const date = Date.now();
+        await User.updateOne({_id: userIds[socket.id]}, {isOnline: false, lastOnline: date, typingTo: ''});
+        await sendUsers();
+        delete userIds[socket.id];
     });
 });
 
--- models.js
+++ models.js
@@ -2,16 +2,34 @@ const {Schema, model} = require('mongoose');
 
 const User = new Schema({
     email: {type: String, required: true, unique: true},
+    username: {type: String, default: 'username'},
+    fullName: {type: String, default: 'full name'},
+    // username: {type: String, unique: true, required: true},
+    // fullName: {type: String, required: true},
     password: {type: String, required: true},
     isActivated: {type: Boolean, default: false},
     activationLink: {type: String},
+    imgSrc: {type: String, default: ''},
+    lastOnline: {type: Number, default: Date.now()},
+    isOnline: {type: Boolean, default: false},
+    typingTo: {type: String, default:''}
+    // typingTo: {type: Schema.Types.ObjectId, ref: 'User'}
 });
+
 const Token = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     refreshToken: {type: String, required: true},
 });
-//
 
+const Message = new Schema({
+    senderId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    receiverId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    sentTime: {type: Number, required: true},
+    msgType: {type: String, required: true},
+    deliveryStatus: {type: Number, default: 1},
+    msgContent: {type: String, required: true}
+});
 
 module.exports.User = model('User', User);
 module.exports.Token = model('Token', Token);
+module.exports.Message = model('Message', Message);
